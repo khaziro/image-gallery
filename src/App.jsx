d4649ed87c6559ab0e2b81214501bf791cc040c6
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-03-19 20:29:08 +0200

    Set-up project with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-center font-bold underline">
+        React Content Set-Up
+      </h1>
+    </div>
+  )
+}
+
+export default App
