0ca0ce11b5ede5d7711db2b0ac5ba238ef94c61b
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-03-20 13:10:54 +0200

    Fetched the data, added the child component inside the parent and set up some states

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import ImageCard from "./components/imageCard.jsx";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('')
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${import.meta.env.VITE_REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false)
+      })
+      .catch(err => console.log(err))
+  }, [])
 
   return (
-    <div className="App">
-      <h1 className="text-3xl text-center font-bold underline">
-        React Content Set-Up
-      </h1>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image}/>
+        ))}
+      </div>
     </div>
   )
 }
