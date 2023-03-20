69bde39ffc42e1856e5753ecddcbfc000ca82dc6
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-03-20 15:44:51 +0200

    Added a div which shows the user there wasn't any image found if his input doesn't match any of the images

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,14 @@ function App() {
   return (
     <div className="container mx-auto">
       <ImageSearch searchText={(text) => setTerm(text)}/>
-      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
-        {images.map(image => (
-          <ImageCard key={image.id}
-                     image={image}/>
-        ))}
-      </div>}
+      {!isLoading && images.length === 0 && <h1 className="text-6xl text-center mx-auto mt-32">No Images Found</h1>}
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> :
+        <div className="grid grid-cols-3 gap-4">
+          {images.map(image => (
+            <ImageCard key={image.id}
+                       image={image}/>
+          ))}
+        </div>}
     </div>
   )
 }
